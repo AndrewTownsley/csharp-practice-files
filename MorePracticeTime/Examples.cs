498c79e521a026af904365920bda68fb62b30e8f
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-26 08:28:52 -0600

    csharp-practice

---
+++ MorePracticeTime/Examples.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorePracticeTime
+{
+    internal class Examples
+    {
+
+
+
+
+
+        /* public static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while(true)
+            {
+                Console.Write("Enter a number (or quit to exit)");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+
+            Console.WriteLine("Unique Numbers: ");
+            foreach(var number in GetUniqueNumbers(numbers))
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetUniqueNumbers(List<int> numbers)
+        {
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+            return uniques; 
+        
+    }*/
+
+
+
+
+     /*   public static void Main(string[] args)
+            {
+                Console.Write("Whats your name?");
+                var name = Console.ReadLine();
+                var reversed = ReverseName(name);
+                Console.WriteLine("Reversed Name: " + ReverseName(name));
+            }
+
+            public static string ReverseName(string name)
+            {
+                var array = new char[name.Length];
+                for (var i = name.Length; i > 0; i--)
+                    array[name.Length - i] = name[i - 1];
+
+                return new string(array);
+            }*/
+
+    }
+}
---
+++ MorePracticeTime/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace MorePracticeTime
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            /// FILES // FILES // FILES //
+            /// 
+            /*
+             *      // File Methods...
+             *      
+             *      File.
+             *      FileInfo.
+             *      
+                    .Create();
+                    .Copy();
+                    .Delete();
+                    .Exists();
+                    .GetAttributes();
+                    .Move();
+                    .ReadAllText();*/
+
+            // File - 
+            /*   var path = @"c:\somefile.jpg"; 
+               File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+               File.Delete(path);
+               if(File.Exists(path))
+               {
+                   // 
+               }
+               var content = File.ReadAllText(path);
+
+               var fileInfo = new FileInfo(path); 
+               fileInfo.CopyTo("...");
+               fileInfo.Delete();
+               if(fileInfo.Exists)
+               {
+                   // 
+               }
+               fileInfo.Read();*/
+
+
+            // Directory.
+            // DirectoryInfo.
+            // CreateDirectory();
+            /*      .Delete();
+                    .Exists();
+                    .GetCurrentDirectory();
+                    .GetFiles();
+                    .Move();
+                    .GetLogicalDrives();*/
+
+
+            /*        Directory.*/
+
+            /*            Directory.CreateDirectory(@"c:\temp\folder1");
+            */      /*      var files = Directory.GetFiles(@"c:\Users\acer\Repos", "*.html*", SearchOption.AllDirectories);  
+                            foreach(var file in files)
+                                Console.WriteLine(file);*/
+            /*
+                        var directories = Directory.GetDirectories(@"c:\Users\acer\Repos", "*.*", SearchOption.AllDirectories);
+                        foreach(var directory in directories)
+                            Console.WriteLine(directory);
+
+                        *//*            Directory.Exists(".. directory path here...");*//*
+
+                        var directoryInfo = new DirectoryInfo("...");
+                        directoryInfo.GetFiles();
+                        directoryInfo.GetDirectories();
+                    //*/
+            //
+            // Path.
+            /*
+                    .GetDirectoryName();
+                    .GetFileName();
+                    .GetExtension();
+                    .GetTempPath();*/
+
+
+            var path = @"C:\Users\acer\Repos\codewars-practice\app.js";
+
+            Console.WriteLine("Extension Path: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name w/o Extensiom: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+
+
+
+
+            /*var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);*/
+
+
+
+        }
+    }
+}
